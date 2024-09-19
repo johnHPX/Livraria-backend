6b3b131fd8769d7729ab4b67d32ad71e926afe58
johnHPX <jonatasfreitas008@gmail.com>
2024-09-19 14:12:03 -0300

    fix main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -3,8 +3,6 @@ import repository.ConexaoBD;
 public class main {
 
     public static void main(String[] args){
-        ConexaoBD conexao = new ConexaoBD();
-        conexao.connectar();
         System.out.println("Hello boy");
     }
 }
