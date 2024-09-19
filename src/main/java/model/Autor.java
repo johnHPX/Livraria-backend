05fd0ab1dfd0590272f47414ebffe390abe67a97
robertscj <robertscj@proton.me>
2024-09-19 20:31:10 -0300

    /model/Autor.java adicionado

---
+++ src/main/java/model/Autor.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author roberts
+ */
+public class Autor {
+    private int id;
+    private String nome;
+    private String bio;
+    private int ano_nasc;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the bio
+     */
+    public String getBio() {
+        return bio;
+    }
+
+    /**
+     * @param bio the bio to set
+     */
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    /**
+     * @return the ano_nasc
+     */
+    public int getAno_nasc() {
+        return ano_nasc;
+    }
+
+    /**
+     * @param ano_nasc the ano_nasc to set
+     */
+    public void setAno_nasc(int ano_nasc) {
+        this.ano_nasc = ano_nasc;
+    }
+   
+    
+}
