df3c3f44708f94c6bf1a45ea6997279f756ac520
robertscj <robertscj@proton.me>
2024-09-19 21:00:50 -0300

    Adicionado arquivo /model/Livro_Autor.java

---
+++ src/main/java/model/Livro_Autor.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author roberts
+ */
+public class Livro_Autor {
+    private int id;
+    private int livro_id;
+    private int autor_id;
+}
