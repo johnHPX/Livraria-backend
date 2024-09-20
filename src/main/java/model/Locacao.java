b26b595fe8a323f7be5d3401c3c53608081d2903
robertscj <robertscj@proton.me>
2024-09-19 21:16:36 -0300

    adicionado arquivo /model/Locacao.java

---
+++ src/main/java/model/Locacao.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author roberts
+ */
+public class Locacao {
+    private int id;
+    private int usuario_id;
+    private int funcionario_id;
+    private int edicao_cod;
+    private String prazo;
+    private String data_locacao;
+    private int ano;
+    private boolean aberta;
+}
