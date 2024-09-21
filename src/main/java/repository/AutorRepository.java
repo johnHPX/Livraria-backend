6e7cfde951000ef3abefd4b8f684ac457f9b29a3
johnHPX <jonatasfreitas008@gmail.com>
2024-09-20 21:44:18 -0300

    criado a classe autorRepository e implementando o metodo criarAutor

--- src/main/java/repository/AutorRepository.java
+++ src/main/java/repository/AutorRepository.java
@@ -1,42 +1,41 @@
-//package repository;
-//
-//import model.Autor;
-//import model.Funcionario;
-//import util.DatasFormatadas;
-//
-//import java.sql.*;
-//import java.time.LocalDate;
-//import java.time.format.DateTimeFormatter;
-//import java.util.ArrayList;
-//
-//public class AutorRepository {
-//
-//    DatasFormatadas dataUSA = DatasFormatadas.PADRAO_USA;
-//
-//    public void criarAutor(Autor autor) {
-//        try {
-//            ConexaoBD conexao = new ConexaoBD();
-//            conexao.connectar();
-//            String sql = "INSERT INTO autor(NOME, CPF, SENHA, DATA_NASC, ENDERECO) values(?, ?, ?, ?, ?)";
-//
-//            PreparedStatement pstmt = conexao.conn.prepareStatement(sql);
-//            pstmt.setString(1, funcionario.getNome());
-//            pstmt.setString(2, funcionario.getCpf());
-//            pstmt.setString(3, funcionario.getSenha());
-//            pstmt.setString(4, funcionario.getData_nasc());
-//            pstmt.setString(5, funcionario.getEndereco());
-//            pstmt.executeUpdate();
-//
-//            pstmt.close();
-//            conexao.fecharConexao();
-//        }
-//        catch (SQLException e) {
-//            System.err.print(e.getMessage());
-//        }
-//
-//    }
-//
-//    public ArrayList<Funcionario> listarTodosFuncionarios(){
+package repository;
+
+import model.Autor;
+import model.Funcionario;
+import util.DatasFormatadas;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+public class AutorRepository {
+
+    DatasFormatadas dataUSA = DatasFormatadas.PADRAO_USA;
+
+    public void criarAutor(Autor autor) {
+        ConexaoBD conexao = new ConexaoBD();
+        try {
+            conexao.connectar();
+            String sql = "INSERT INTO autor(NOME, BIO, ANO_NASC) values(?, ?, ?)";
+
+            PreparedStatement pstmt = conexao.conn.prepareStatement(sql);
+            pstmt.setString(1,autor.getNome());
+            pstmt.setString(2, autor.getBio());
+            pstmt.setInt(3, autor.getAno_nasc());
+            pstmt.executeUpdate();
+
+            pstmt.close();
+        }
+        catch (SQLException e) {
+            System.err.print(e.getMessage());
+        }finally {
+            conexao.fecharConexao();
+        }
+
+    }
+
+//    public ArrayList<Autor> listarTodosAutor(){
 //        ConexaoBD conexaoBD = new ConexaoBD();
 //        ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
 //        try{
@@ -65,51 +64,51 @@
 //        }
 //        return funcionarios;
 //    }
-//
-//    public void alterarFuncionario(Funcionario funcionario) {
-//        ConexaoBD conexaoBD = new ConexaoBD();
-//        try {
-//            String sqlText = "UPDATE funcionario SET NOME = ?, CPF = ?, SENHA = ?, ENDERECO = ?, DATA_NASC = ? WHERE ID = ?";
-//            conexaoBD.connectar();
-//            PreparedStatement pstmt = conexaoBD.conn.prepareStatement(sqlText);
-//            pstmt.setInt(6, funcionario.getId());
-//            pstmt.setString(1, funcionario.getNome());
-//            pstmt.setString(2, funcionario.getCpf());
-//            pstmt.setString(3, funcionario.getSenha());
-//            pstmt.setString(4, funcionario.getEndereco());
-//
-//            // Conveting string to Date
-//            LocalDate localDate = LocalDate.parse(funcionario.getData_nasc(), DateTimeFormatter.ofPattern(dataUSA.getValor()));
-//            Date sqlDate = Date.valueOf(localDate);
-//
-//            pstmt.setDate(5, sqlDate);
-//            pstmt.executeUpdate();
-//
-//            pstmt.close();
-//
-//        } catch (SQLException e) {
-//            System.err.println(e.getMessage());
-//        } finally {
-//            conexaoBD.fecharConexao();
-//        }
-//    }
-//
-//    public void removerFuncionario(int id){
-//        ConexaoBD conexaoDB = new ConexaoBD();
-//        try{
-//            String sql = "DELETE FROM funcionario WHERE ID = ?";
-//            conexaoDB.connectar();
-//
-//            PreparedStatement pstmt = conexaoDB.conn.prepareStatement(sql);
-//            pstmt.setInt(1, id);
-//            pstmt.executeUpdate();
-//
-//            pstmt.close();
-//        }catch (SQLException e){
-//            System.err.println(e.getMessage());
-//        }finally {
-//            conexaoDB.fecharConexao();
-//        }
-//    }
-//
-//}
+
+    public void alterarFuncionario(Funcionario funcionario) {
+        ConexaoBD conexaoBD = new ConexaoBD();
+        try {
+            String sqlText = "UPDATE funcionario SET NOME = ?, CPF = ?, SENHA = ?, ENDERECO = ?, DATA_NASC = ? WHERE ID = ?";
+            conexaoBD.connectar();
+            PreparedStatement pstmt = conexaoBD.conn.prepareStatement(sqlText);
+            pstmt.setInt(6, funcionario.getId());
+            pstmt.setString(1, funcionario.getNome());
+            pstmt.setString(2, funcionario.getCpf());
+            pstmt.setString(3, funcionario.getSenha());
+            pstmt.setString(4, funcionario.getEndereco());
+
+            // Conveting string to Date
+            LocalDate localDate = LocalDate.parse(funcionario.getData_nasc(), DateTimeFormatter.ofPattern(dataUSA.getValor()));
+            Date sqlDate = Date.valueOf(localDate);
+
+            pstmt.setDate(5, sqlDate);
+            pstmt.executeUpdate();
+
+            pstmt.close();
+
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            conexaoBD.fecharConexao();
+        }
+    }
+
+    public void removerFuncionario(int id){
+        ConexaoBD conexaoDB = new ConexaoBD();
+        try{
+            String sql = "DELETE FROM funcionario WHERE ID = ?";
+            conexaoDB.connectar();
+
+            PreparedStatement pstmt = conexaoDB.conn.prepareStatement(sql);
+            pstmt.setInt(1, id);
+            pstmt.executeUpdate();
+
+            pstmt.close();
+        }catch (SQLException e){
+            System.err.println(e.getMessage());
+        }finally {
+            conexaoDB.fecharConexao();
+        }
+    }
+
+}
